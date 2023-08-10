cf54e87d46521b4cb45beb9477407851e82c506e
itsdalia <dlooshmohamed118@gmail.com>
2023-08-10 16:13:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: always 1 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
