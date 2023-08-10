982634992a6734671bed74a2be8660c2e6babaf6
itsdalia <dlooshmohamed118@gmail.com>
2023-08-10 13:14:52 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programe that print a line using printf fuction
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
+}
