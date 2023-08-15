20f40adbbd0102c9cb1f340c3eb187a2ce44f173
itsdalia <dlooshmohamed118@gmail.com>
2023-08-15 06:27:41 -0700

    ./2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
